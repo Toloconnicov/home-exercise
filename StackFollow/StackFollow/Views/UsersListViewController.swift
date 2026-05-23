271c9c8ce3a8e9c2b8d1373bce2d942e1324e31c
Toloconnicov <Toloconnicov@users.noreply.github.com>
2026-05-23 15:01:58 +0300

    project setup

---
+++ StackFollow/StackFollow/Views/UsersListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StackFollow
+//
+//  Created by Mac on 23.05.2026.
+//
+
+import UIKit
+
+class UsersListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
